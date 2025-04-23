9c9ce35bc4f81cb5b1ed5cd3ac80418a4caaf090
Jesus Hincapie <jahlon@outlook.com>
2025-04-23 13:38:28 -0500

    Add comments to interface

--- app/interfaces.py
+++ app/interfaces.py
@@ -5,8 +5,18 @@ class ICorrector(ABC):
 
     @abstractmethod
     def corregir_ortografia(self, texto: str) -> str:
+        """
+        Este método recibe una cadena de texto y retorna la cadena con cualquier error de ortografía corregido
+        :param texto: La cadena a corregir
+        :return: El texto corregido
+        """
         ...
 
     @abstractmethod
     def obtener_cantidad_errores(self, texto: str) -> int:
+        """
+        Este método recibe una cadena de texto y retorna un entero con la cantidad de errores ortográficos
+        :param texto: La cadena a corregir
+        :return: La cantidad de errores
+        """
         ...
